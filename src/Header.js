2b88b57697d39d48ea06f0d6153da7df3f4dcfd0
William Lorey <wwlorey@gmail.com>
2019-03-28 21:18:22 -0500

    Add typed name animation

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,63 @@
 import React, { Component } from 'react';
+import Typist from 'react-typist';
 import './Header.css';
 
+// Typed name states
+const INIT_NICKNAME = 0;
+const FULL_NAME = 1;
+const NICKNAME = 2;
+
+const cursor = {
+  show: true,
+  blink: false,
+  element: '|',
+  hideWhenDone: true,
+  hideWhenDoneDelay: 300,
+};
+
+function TypedName(props) {
+  if (props.nameState === INIT_NICKNAME) {
+    return <Typist cursor={cursor}>Will</Typist>;
+  } else if (props.nameState === FULL_NAME) {
+    return (
+      <div>
+        Will
+        <Typist cursor={cursor} onTypingDone={props.onTypingDone}>
+          iam Lorey
+          <Typist.Backspace count={9} delay={800} />
+        </Typist>
+      </div>
+    );
+  } else {
+    // props.nameState === NICKNAME
+    return <div>Will</div>;
+  }
+}
+
 export default class Header extends Component {
+  state = {
+    nameState: INIT_NICKNAME,
+  };
+
+  expandName = () => {
+    this.setState({ nameState: FULL_NAME });
+  };
+
+  shrinkName = () => {
+    this.setState({ nameState: NICKNAME });
+  };
+
   render() {
-    return <div className="Header">header text</div>;
+    return (
+      <div className="header">
+        <div className="header__hello">Hi there, I'm</div>
+        <div className="header__name" onMouseEnter={this.expandName}>
+          <TypedName
+            nameState={this.state.nameState}
+            onTypingDone={this.shrinkName}
+          />
+        </div>
+      </div>
+    );
   }
 }
