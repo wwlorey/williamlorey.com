1d4d7b4c7ed30f8e23f499362d18e6d07de2881d
William Lorey <wwlorey@gmail.com>
2019-03-30 19:40:55 -0500

    Animate pic links

--- src/components/PicLink.js
+++ src/components/PicLink.js
@@ -1,19 +1,27 @@
 import React from 'react';
+import ScrollAnimation from 'react-animate-on-scroll';
 import './PicLink.css';
 
 export default function PicLink(props) {
   return (
     <div className="pic-link-wrapper">
-      <a href={props.link} target="_blank">
-        <div
-          style={{
-            backgroundImage:
-              'url(' + require('../assets/images/' + props.pic) + ')',
-          }}
-          className="pic"
-        />
-        <div className="label">{props.label}</div>
-      </a>
+      <ScrollAnimation
+        animateIn="animate-pic-link"
+        delay={1}
+        duration={1.2}
+        animatePreScroll={false}
+      >
+        <a href={props.link} target="_blank">
+          <div
+            style={{
+              backgroundImage:
+                'url(' + require('../assets/images/' + props.pic) + ')',
+            }}
+            className="pic"
+          />
+          <div className="label">{props.label}</div>
+        </a>
+      </ScrollAnimation>
     </div>
   );
 }
