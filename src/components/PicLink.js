2a1aac736463ab2e116f8c5b64f5177c707b9882
William Lorey <wwlorey@gmail.com>
2019-03-31 18:14:44 -0500

    Animate pic links on hover

--- src/components/PicLink.js
+++ src/components/PicLink.js
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './PicLink.css';
 
-export default function PicLink(props) {
-  return (
-    <div className={`pic-link-wrapper ${props.className}`}>
-      <a href={props.link} target="_blank" rel="noopener noreferrer">
-        <div
-          style={{
-            backgroundImage:
-              'url(' + require('../assets/images/' + props.pic) + ')',
-          }}
-          className="pic"
-        />
-        <div className="label">{props.label}</div>
-      </a>
-    </div>
-  );
+export default class PicLink extends Component {
+  constructor(props) {
+    super(props);
+    this.resizeTimer = null;
+  }
+
+  state = {
+    transitions: true,
+  };
+
+  handleResize = () => {
+    // Prevent goofy transitions while resizing
+    this.setState({ transitions: false });
+
+    clearTimeout(this.resizeTimer);
+    this.resizeTimer = setTimeout(() => {
+      this.setState({ transitions: true });
+    }, 200);
+  };
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+  }
+
+  render() {
+    return (
+      <div
+        className={`pic-link-wrapper ${this.props.className} ${
+          this.state.transitions ? '' : 'no-transitions'
+        }`}
+      >
+        <a href={this.props.link} target="_blank" rel="noopener noreferrer">
+          <div
+            style={{
+              backgroundImage:
+                'url(' + require('../assets/images/' + this.props.pic) + ')',
+            }}
+            className="pic"
+          />
+          <div className="label">{this.props.label}</div>
+        </a>
+      </div>
+    );
+  }
 }
