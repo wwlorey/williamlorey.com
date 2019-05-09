f7a8bb042f230d2f8a265032c0df44062b23d455
William Lorey <will@williamlorey.com>
2019-05-09 15:26:47 -0500

    Remove hover on expansion links

--- src/components/PicLink.js
+++ src/components/PicLink.js
@@ -1,57 +1,26 @@
 import React, { Component } from 'react';
 import './PicLink.css';
 
-export default class PicLink extends Component {
-  constructor(props) {
-    super(props);
-    this.resizeTimer = null;
-  }
-
-  state = {
-    transitions: true
-  };
-
-  handleResize = () => {
-    // Prevent goofy transitions while resizing
-    this.setState({ transitions: false });
-
-    clearTimeout(this.resizeTimer);
-    this.resizeTimer = setTimeout(() => {
-      this.setState({ transitions: true });
-    }, 200);
-  };
-
-  componentDidMount() {
-    window.addEventListener('resize', this.handleResize);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.handleResize);
-  }
-
-  render() {
-    return (
-      <div
-        className={`pic-link-wrapper ${this.props.className} ${
-          this.state.transitions ? '' : 'no-transitions'
-        }`}
+function PicLink(props) {
+  return (
+    <div className={`pic-link-wrapper ${props.className}`}>
+      <a
+        href={props.link}
+        target={props.newTab ? '_blank' : ''}
+        rel={props.newTab ? 'noopener noreferrer' : ''}
+        className={'pic-link-link'}
       >
-        <a
-          href={this.props.link}
-          target={this.props.newTab ? '_blank' : ''}
-          rel={this.props.newTab ? 'noopener noreferrer' : ''}
-          className={'pic-link-link'}
-        >
-          <div
-            style={{
-              backgroundImage:
-                'url(' + require('../assets/images/' + this.props.pic) + ')'
-            }}
-            className='pic'
-          />
-          <div className='label'>{this.props.label}</div>
-        </a>
-      </div>
-    );
-  }
+        <div
+          style={{
+            backgroundImage:
+              'url(' + require('../assets/images/' + props.pic) + ')'
+          }}
+          className='pic'
+        />
+        <div className='label'>{props.label}</div>
+      </a>
+    </div>
+  );
 }
+
+export default PicLink;
