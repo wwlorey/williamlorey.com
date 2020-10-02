1b62f55011e67ae2e99329f4f48d05e8a80f479d
William Lorey <will@williamlorey.com>
2020-10-02 11:41:40 -0700

    Add post template script, post header generation

---
+++ blog/getPostTemplate.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+echo -n "Title: "
+read TITLE
+
+echo -n "Date (YYYY-MM-DD): "
+read DATE
+
+while [ 1 ]
+do
+    echo -n "HTML path (file-path.html): "
+    read FILE_PATH
+    if test -f "$FILE_PATH"; then
+        echo "Error: \"$FILE_PATH\" already exists."
+    else
+        break
+    fi
+done
+
+echo "<!DOCTYPE html>
+<html>
+    <head>
+        <!-- DATE: $DATE-->
+        <meta charset='utf-8'>
+        <title>$TITLE</title>
+        <link rel='stylesheet' href='../stylesheet.css' type='text/css'>
+        <link rel='icon' type='image/png' href='../images/love.png'>
+    </head>
+    <body>
+        <h3>$TITLE</h3>
+
+        <p>- <a href='../index.html'>Will</a></p>
+    </body>
+</html>" > $FILE_PATH
--- blog/populatePosts.py
+++ blog/populatePosts.py
@@ -7,14 +7,21 @@ END_POSTS_STRING = 'END POSTS'
 DATE_STRING = 'DATE:'
 TITLE_TAG = '<title>'
 BLOG_HOME_PATH = 'index.html'
-
-def getPostLi(date, filePath, title):
-    return '<li>%s: <a href="%s">%s</a></li>' % (date, filePath, title)
-
-def getTag(html, tag):
-    start = html.find(tag)
-    end = html.find('<', start + 1)
-    return html[start+len(tag):end]
+MONTH_NAMES = [
+    None,
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+]
 
 def getComment(html, commentId):
     comment = '<!-- %s' % commentId
@@ -22,6 +29,31 @@ def getComment(html, commentId):
     end = html.find('-->', start + 1)
     return html[start+len(comment):end].replace(' ', '') # Dates shouldn't include spaces
 
+def getTag(html, tag):
+    start = html.find(tag)
+    end = html.find('<', start + 1)
+    return html[start+len(tag):end]
+
+def getMonth(dateString):
+    return dateString[:len('YYYY-MM')]
+
+def getMonthHeading(monthString):
+    year = monthString[:len('YYYY')]
+    month = int(monthString[-2:])
+    monthString = MONTH_NAMES[month]
+    print('Found post(s) from %s %s' % (year, monthString))
+    return '<h4>%s %s</h4>\n<ul>' % (year, monthString)
+
+class Post:
+    def __init__(self, date, filePath, title):
+        self.date = date
+        self.filePath = filePath
+        self.title = title
+
+    def getPostHtml(self):
+        return '<li>%s: <a href="%s">%s</a></li>' % (self.date, self.filePath, self.title)
+
+''' Get insert start and end indices '''
 blogHomeContent = open(BLOG_HOME_PATH, 'r').read().split('\n')
 for index, line in enumerate(blogHomeContent):
     if BEGIN_POSTS_STRING in line:
@@ -35,14 +67,35 @@ print('Insert end:', insertEnd)
 
 blogHomeContent = blogHomeContent[:insertStart] + blogHomeContent[insertEnd:]
 
-position = insertStart
+''' Generate and sort Post objects '''
 postFiles = [f for f in os.listdir('.') if '.html' in f and f != BLOG_HOME_PATH]
+posts = []
 for postFile in postFiles:
     postContent = open(postFile, 'r').read()
     date = getComment(postContent, DATE_STRING)
     title = getTag(postContent, TITLE_TAG)
-    blogHomeContent.insert(position, getPostLi(date, postFile, title))
+    posts.append(Post(date, postFile, title))
+
+posts.sort(key=lambda post: post.date, reverse=True)
+
+''' Write posts '''
+position = insertStart
+prevMonth = None
+for post in posts:
+    currentMonth = getMonth(post.date)
+    if currentMonth != prevMonth:
+        if prevMonth != None:
+            blogHomeContent.insert(position, '</ul>\n')
+            position += 1
+        
+        blogHomeContent.insert(position, getMonthHeading(currentMonth))
+        position += 1
+        prevMonth = currentMonth
+
+    blogHomeContent.insert(position, post.getPostHtml())
     position += 1
 
+blogHomeContent.insert(position, '</ul>\n')
+
 open(BLOG_HOME_PATH, 'w').write('\n'.join(blogHomeContent))
-print('Wrote %i blog posts to %s' % (len(postFiles), BLOG_HOME_PATH))
+print('Wrote %i blog post(s) to %s' % (len(postFiles), BLOG_HOME_PATH))
